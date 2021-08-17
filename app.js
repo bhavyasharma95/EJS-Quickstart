056ed267e4f61f35baf7c9c9db4e66342784f850
beneadroit <bhavya1234sharma@gmail.com>
2021-08-18 00:16:20 +0530

    added auth and message rendering

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const { ppid } = require("process");
+
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express()
 
@@ -9,41 +12,117 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 
+app.use(session({
+    secret: 'Flymetothemoon',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/toxicDB", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
-    username: String,
     email: String,
     password: String
 })
 
+const toxicSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+})
+
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema)
+const Toxic = new mongoose.model("Toxic", toxicSchema)
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // routes
 
 app.route("/")
 .get(function(req,res){
-    res.render("home")
+    Toxic.find({}, function(err, found){
+        res.render("home",{
+            posts: found,
+        })
+    })
+})
+
+app.route("/compose")
+.get(function(req,res){
+    if(req.isAuthenticated()){
+        res.render("compose",{
+            title: "Be Toxic"
+        })
+    }else{
+        res.redirect("/login")
+    }
+})
+.post(function(req,res){
+    const toxic = new Toxic({
+        title:req.body.postTitle,
+        body:req.body.postContent,
+    })
+    toxic.save()
+    res.redirect("/")
+})
+
+app.route("/community")
+.get(function(req,res){
+    if(req.isAuthenticated()){
+        res.render("community")
+    }else{
+        res.redirect("/login")
+    }
 })
 
 app.route("/login")
 .get(function(req,res){
     res.render("login")
 })
+.post(function(req,res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, function(err){
+        if(!err){
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/compose");
+            })
+        }
+    })
+})
 
 app.route("/signup")
 .get(function(req,res){
     res.render("signup")
 })
-
-app.route("/compose")
-.get(function(req,res){
-    res.render("compose")
+.post(function(req,res){
+    User.register({username: req.body.username}, req.body.password, function(err,user){
+        if(err){
+            console.log("Please, try again...");
+            res.redirect("/signup")
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/compose");
+            })
+        }
+    })
 })
 
-app.route("/community")
+app.route("/logout")
 .get(function(req,res){
-    res.render("community")
+    req.logOut();
+    res.redirect("/");
 })
 
 //listen
